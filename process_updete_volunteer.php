0bd8926f29fbfc4a20149b5441eff9dee4fcc8a5
hafis825 <150876306+hafis825@users.noreply.github.com>
2024-02-06 14:52:43 +0700

    process_updete_volunteer.php

--- process_updete_volunteer.php
+++ process_updete_volunteer.php
@@ -2,23 +2,24 @@
     include "config.php";
 
     
-    $id = mysqli_real_escape_string($conn, $_POST['id']);
-    $volunteer = mysqli_real_escape_string($conn, $_POST['volunteer']);
-    $location = mysqli_real_escape_string($conn, $_POST['location']);
-    $hours = mysqli_real_escape_string($conn, $_POST['hours']);
-    $event_date = mysqli_real_escape_string($conn, $_POST['event_date']);
+   $volunteer = $_POST['volunteer'];
+   $location = $_POST['location'];
+   $hours = $_POST['hours'];
+   $event_date = $_POST['event_date'];
+   $id = $_POST['id'];
    
 
-   $sql = "UPDATE todo SET volunteer='$volunteer', location='$location', hours='$hours', event_date='$event_date' WHERE id='$id'";
-   $qry = mysqli_query($conn,$sql); 
-   if (!$qry) {
-       echo "ไม่สามารถเพิ่มข้อมูลได้";
-       header("location: update_volunteer.php");
-   }else { 
-        // เพิ่มข้อมูลเรียบร้อยแล้ว
+    $sql = "UPDATE todo SET volunteer='$volunteer', location='$location', hours='$hours', event_date='$event_date' WHERE id='$id'";
+    $qry = mysqli_query($conn, $sql); 
+    
+    if (!$qry) {
+        // อัปเดตข้อมูลไม่สำเร็จ
+        header("location: update_volunteer.php");
+    } else { 
+        // อัปเดตข้อมูลเรียบร้อยแล้ว
         header("location: volunteer.php");
-   }
-
-   mysqli_close($conn);
+    }
+    
+    mysqli_close($conn);
 
 ?>
